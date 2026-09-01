e0a1dac9693027f6b820c6f0ae7cc4cc08d2dd1d
Bruno-AngeloBr <acer2005br@gmail.com>
2026-08-31 23:00:51 -0300

    Alterando as primeiras dependencias

--- src/main/java/br/edu/usc/campusiachatbot/entity/AtendimentoEntity.java
+++ src/main/java/br/edu/usc/campusiachatbot/entity/AtendimentoEntity.java
@@ -4,16 +4,8 @@ import br.edu.usc.campusiachatbot.enums.CategoriaAtendimentoEnum;
 import br.edu.usc.campusiachatbot.enums.OrigemMensagemEnum;
 import br.edu.usc.campusiachatbot.enums.StatusAtendimentoEnum;
 import br.edu.usc.campusiachatbot.enums.TipoSolicitacaoEnum;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,56 +13,38 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "atendimentos")
+@Document
 @Getter
 @Setter
 public class AtendimentoEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    @Column(nullable = false, length = 20)
     private String telefoneCliente;
 
-    @Column(length = 120)
     private String nomeCliente;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 30)
     private OrigemMensagemEnum origem;
 
-    @Column(nullable = false, length = 4000)
     private String mensagemCliente;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 40)
     private TipoSolicitacaoEnum tipoSolicitacao;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 40)
     private CategoriaAtendimentoEnum categoria;
 
-    @Column(length = 4000)
     private String respostaGerada;
 
-    @Column(nullable = false)
     private boolean necessitaAtendimentoHumano;
 
-    @Column(length = 1000)
     private String motivoEncaminhamento;
 
     private Double confianca;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 40)
     private StatusAtendimentoEnum status;
 
-    @Column(nullable = false)
     private LocalDateTime dataProcessamento;
 
-    @OneToMany(mappedBy = "atendimento", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MensagemAtendimentoEntity> mensagens = new ArrayList<>();
 
 }
--- src/main/java/br/edu/usc/campusiachatbot/entity/MensagemAtendimentoEntity.java
+++ src/main/java/br/edu/usc/campusiachatbot/entity/MensagemAtendimentoEntity.java
@@ -1,44 +1,27 @@
 package br.edu.usc.campusiachatbot.entity;
 
 import br.edu.usc.campusiachatbot.enums.DirecaoMensagemEnum;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "mensagens_atendimento")
+@Document
 @Getter
 @Setter
 public class MensagemAtendimentoEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "atendimento_id", nullable = false)
     private AtendimentoEntity atendimento;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 20)
     private DirecaoMensagemEnum direcao;
 
-    @Column(nullable = false, length = 4000)
     private String conteudo;
 
-    @Column(nullable = false)
     private LocalDateTime dataMensagem;
 
 }
